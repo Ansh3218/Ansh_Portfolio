132918082b55344d487e46e61dd13bda1899edad
CA_THE_FULL_STACKER <vijendrasingh88905@gmail.com>
2025-12-22 19:30:53 +0530

    react-compilar-runtime

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // Remove the experimental.reactCompiler option
 };
 
 export default nextConfig;
